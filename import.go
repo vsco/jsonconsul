9fea763fac7c7ce71eef62c545087e7a784c8a9e
Abhi Yerra <abhi@berkeley.edu>
2015-06-03 16:18:58 -0700

    Actually upload the keys

--- import.go
+++ import.go
@@ -6,9 +6,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	//	"os"
+	"github.com/hashicorp/consul/api"
 )
 
 type JsonImport struct {
+	// Prefix to load the config under. If empty then loads to the
+	// root kv node.
+	Prefix string
 	// File containing the Json
 	Filename string
 
@@ -31,11 +35,7 @@ func (ji *JsonImport) keysFromJson(nested interface{}, prefix string) {
 	}
 }
 
-func (ji *JsonImport) Run() {
-	var unmarshalled map[string]interface{}
-
-	ji.flattened = make(map[string]string)
-
+func (ji *JsonImport) readFile() (unmarshalled map[string]interface{}) {
 	fileOutput, err := ioutil.ReadFile(ji.Filename)
 	if err != nil {
 		panic(err)
@@ -46,7 +46,34 @@ func (ji *JsonImport) Run() {
 		panic(err)
 	}
 
-	ji.keysFromJson(unmarshalled, "")
+	return
+}
 
-	// Set the consul values
+func (ji *JsonImport) prefixedKey(key string) string {
+	return fmt.Sprintf("%s/%s", ji.Prefix, key)
+}
+
+func (ji *JsonImport) setConsulValues() {
+	client, _ := api.NewClient(api.DefaultConfig())
+	kv := client.KV() // Lookup the pair
+
+	for k, v := range ji.flattened {
+		p := &api.KVPair{
+			Key:   ji.prefixedKey(k),
+			Value: []byte(v),
+		}
+
+		_, err := kv.Put(p, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (ji *JsonImport) Run() {
+	unmarshalled := ji.readFile()
+
+	ji.flattened = make(map[string]string)
+	ji.keysFromJson(unmarshalled, "")
+	ji.setConsulValues()
 }
